26d32465af1289bb662b6c9a55727cdbdf3faae9
Nikoloz Chavchavadze <nikolozi.chavchavadze@gmail.com>
2026-07-09 14:02:00 +0400

    Testing footers

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from fpdf import FPDF
 
 df = pd.read_csv("topics.csv")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
-    print(row)
     pdf.add_page()
+    pages = row["Pages"]
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(139, 143, 142)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 22, 200, 22)
+
+    # Set the footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+    for i in range(pages - 1):
+        pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("Output.pdf")
